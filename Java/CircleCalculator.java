295b4a4144169cbf040bdb903ed38fe258cf16c4
carlosaculsan-ui <carlosaculsan@gmail.com>
2026-02-10 13:22:52 +0800

    Create CircleCalculator.java

---
+++ Java/CircleCalculator.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class MachineProblem8 {
+    public static void main(String[] args) {
+        Scanner gg = new Scanner(System.in);
+
+        System.out.println("Enter radius area ");
+        double a = gg.nextDouble();
+        System.out.println("Area: " + 2 * Math.PI * a);
+        System.out.println("Circumference: " + a * Math.PI);
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
