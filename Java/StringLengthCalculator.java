5c260f64b27e57f73f5f8933b661ea9a861b4425
carlosaculsan-ui <carlosaculsan@gmail.com>
2026-02-10 13:27:37 +0800

    Create StringLengthCalculator.java

---
+++ Java/StringLengthCalculator.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class MachineProblem17 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Input String: ");
+        String input = scanner.next();
+        
+        int length = input.length();
+        
+        System.out.println("The length of " + input + " is " + length);
+    } 
+}
+   
