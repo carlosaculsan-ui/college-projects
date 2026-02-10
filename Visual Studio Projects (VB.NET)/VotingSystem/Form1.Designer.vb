f828bb8222adc101df6fa81259a818ea73902fdd
carlosaculsan-ui <carlosaculsan@gmail.com>
2026-02-10 19:00:35 +0800

    Add files via upload

---
+++ Visual Studio Projects (VB.NET)/VotingSystem/Form1.Designer.vb
@@ -0,0 +1,162 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        PictureBox1 = New PictureBox()
+        GroupBox1 = New GroupBox()
+        Label3 = New Label()
+        CheckBox1 = New CheckBox()
+        Button1 = New Button()
+        TextBox2 = New TextBox()
+        TextBox1 = New TextBox()
+        Label2 = New Label()
+        Label1 = New Label()
+        Timer1 = New Timer(components)
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        GroupBox1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(68, 28)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(225, 225)
+        PictureBox1.SizeMode = PictureBoxSizeMode.AutoSize
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(Label3)
+        GroupBox1.Controls.Add(CheckBox1)
+        GroupBox1.Controls.Add(Button1)
+        GroupBox1.Controls.Add(TextBox2)
+        GroupBox1.Controls.Add(TextBox1)
+        GroupBox1.Controls.Add(Label2)
+        GroupBox1.Controls.Add(Label1)
+        GroupBox1.Location = New Point(15, 256)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(333, 225)
+        GroupBox1.TabIndex = 1
+        GroupBox1.TabStop = False
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI Emoji", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label3.Location = New Point(255, 206)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(47, 16)
+        Label3.TabIndex = 6
+        Label3.Text = "Label3"
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(273, 68)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(55, 19)
+        CheckBox1.TabIndex = 5
+        CheckBox1.Text = "Show"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI Emoji", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.Location = New Point(111, 112)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(99, 56)
+        Button1.TabIndex = 4
+        Button1.Text = "Login"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(145, 64)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(122, 23)
+        TextBox2.TabIndex = 3
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(145, 19)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(122, 23)
+        TextBox1.TabIndex = 2
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI Emoji", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(29, 63)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(79, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "Password"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI Emoji", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(29, 22)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(83, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "Username"
+        ' 
+        ' Timer1
+        ' 
+        Timer1.Interval = 1000
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        ClientSize = New Size(362, 486)
+        Controls.Add(GroupBox1)
+        Controls.Add(PictureBox1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Timer1 As Timer
+
+End Class
---
+++ Visual Studio Projects (VB.NET)/VotingSystem/Form1.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Reflection.Emit
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Start()
+        Dim Cambria As New Font("Cambria", 12, FontStyle.Regular)
+
+        TextBox1.Font = Cambria
+        TextBox2.Font = Cambria
+        TextBox2.PasswordChar = "*"
+
+
+        GroupBox1.BackColor = Color.Transparent
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Label3.Text = DateTime.Now.ToString("hh:mm:ss tt")
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "admin" And TextBox2.Text = "password" Then
+            Registration_Section.Show()
+            Me.Hide()
+        Else
+
+            MsgBox("Please enter the right username and password", MsgBoxStyle.Exclamation, MessageBoxButtons.OK)
+        End If
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked Then
+            TextBox2.PasswordChar = ""
+        Else
+            TextBox2.PasswordChar = "*"
+        End If
+    End Sub
+End Class
