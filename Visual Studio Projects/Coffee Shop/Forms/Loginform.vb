63f32be8e80a319c24be45549771d597a1cea24b
carlosaculsan-ui <carlosaculsan@gmail.com>
2026-02-10 14:14:48 +0800

    Add files via upload

---
+++ Visual Studio Projects/Coffee Shop/Forms/Loginform.vb
@@ -0,0 +1,79 @@
+﻿Public Class Loginform
+
+
+    Private Const LOCKOUT_DURATION As Integer = 10
+    Private lockoutStartTime As DateTime
+    Private failedAttempts As Integer = 0
+    Private Sub Loginform_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox2.PasswordChar = "*"
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim stUsername As String = TextBox1.Text
+        Dim stPassword As String = TextBox2.Text
+
+        If String.IsNullOrEmpty(stUsername) AndAlso String.IsNullOrEmpty(stPassword) Then
+            MsgBox("Username and password cannot be empty", MessageBoxButtons.OK)
+            Return
+        End If
+
+        If String.IsNullOrEmpty(stUsername) Then
+            MsgBox("User account cannot be empty", MessageBoxButtons.OK)
+            Return
+        End If
+
+        If String.IsNullOrEmpty(stPassword) Then
+            MsgBox("Password cannot be empty", MessageBoxButtons.OK)
+            Return
+        End If
+
+        If stUsername.Length < 4 Then
+            MsgBox("Username cannot be less than 4 characters", MessageBoxButtons.OK)
+            Return
+        End If
+
+        If stUsername = "admin" AndAlso stPassword = "password" Then
+            MessageBox.Show("LOGIN SUCCESSFUL", "Press OK", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Form1.Show()
+            Me.Hide()
+        Else
+            failedAttempts += 1
+            MsgBox("Incorrect Username or Password", MsgBoxStyle.OkOnly, "Invalid")
+
+            If failedAttempts >= 3 Then
+                MsgBox("You are locked out for 10 seconds due to multiple failed login attempts.", MsgBoxStyle.Information, "Locked Out")
+                Button1.Enabled = False
+                LockoutTimer.Interval = 10000
+                LockoutTimer.Start()
+            End If
+
+
+        End If
+    End Sub
+
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked Then
+            TextBox2.PasswordChar = ""
+        Else
+            TextBox2.PasswordChar = "*"
+        End If
+    End Sub
+    Private Sub LockLoginForm()
+
+        Button1.Enabled = False
+        lockoutStartTime = DateTime.Now
+        LockoutTimer.Start()
+    End Sub
+
+    Private Sub LockoutTimer_Tick(sender As Object, e As EventArgs) Handles LockoutTimer.Tick
+        LockoutTimer.Stop()
+        Button1.Enabled = True
+        failedAttempts = 0
+    End Sub
+
+
+
+End Class
---
+++ Visual Studio Projects/Coffee Shop/Forms/Receipt.vb
@@ -0,0 +1,37 @@
+﻿Public Class Receipt
+    Public Sub PopulateReceipt(items As List(Of String), quantities As List(Of Integer), prices As List(Of Double), totalAmount As Double, takeoutOrDineIn As String)
+
+        DataGridView1.Rows.Clear()
+
+
+        DataGridView1.Columns.Clear()
+        DataGridView1.Columns.Add("Item", "Item")
+        DataGridView1.Columns.Add("Quantity", "Quantity")
+        DataGridView1.Columns.Add("Price", "Price")
+
+
+        Dim total As Double = 0
+        For i As Integer = 0 To items.Count - 1
+            Dim subtotal As Double = quantities(i) * prices(i)
+            Dim row As String() = {items(i), quantities(i).ToString(), prices(i).ToString("0.00") & " (" & subtotal.ToString("0.00") & ")"}
+            DataGridView1.Rows.Add(row)
+            total += subtotal
+        Next
+
+
+        Dim totalRow As String() = {"Total Amount:", "", total.ToString("0.00")}
+        DataGridView1.Rows.Add(totalRow)
+
+
+        Dim paymentRow As String() = {"Payment:", "", Form1.iPayment.ToString("0.00")}
+        DataGridView1.Rows.Add(paymentRow)
+
+
+        Dim changeRow As String() = {"Change:", "", Form1.change.ToString("0.00")}
+        DataGridView1.Rows.Add(changeRow)
+
+
+        Dim takeoutOrDineInRow As String() = {"STATUS:", "", takeoutOrDineIn}
+        DataGridView1.Rows.Add(takeoutOrDineInRow)
+    End Sub
+End Class 
