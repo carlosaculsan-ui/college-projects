ac619183b884b5881a2299ce3d887e298a9d25fd
carlosaculsan-ui <carlosaculsan@gmail.com>
2026-02-10 14:42:42 +0800

    Add files via upload

---
+++ Visual Studio Projects/Quiz app/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+
+        If rbMath.Checked Then
+            Dim mathForm As New MathForm()
+            mathForm.ShowDialog()
+        ElseIf rbHistory.Checked Then
+            Dim historyForm As New HistoryForm()
+            historyForm.ShowDialog()
+        ElseIf rbComputerScience.Checked Then
+            Dim computerScienceForm As New ComputerScienceForm()
+            computerScienceForm.ShowDialog()
+        Else
+            MessageBox.Show("Please select a topic.")
+        End If
+    End Sub
+End Class
