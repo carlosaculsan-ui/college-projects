f577aa4abce073651929f73a1a72d5dfd0433643
carlosaculsan-ui <carlosaculsan@gmail.com>
2026-02-10 13:21:05 +0800

    Create SwapNumbers.java

---
+++ Java/SwapNumbers.java
@@ -0,0 +1,25 @@
+public class MachineProblem5 {
+    public static void main(String[] args) {
+        int x = 100;
+        int y = 200;
+
+        System.out.println("THe value of x is " + x);
+        System.out.println("The value of y is " + y);
+
+        int temp = x;
+        x = y;
+        y = temp;
+
+        System.out.println("THe value of x is " + x);
+        System.out.println("The value of y is " + y);
+
+    
+
+
+
+
+
+    }
+
+
+}
