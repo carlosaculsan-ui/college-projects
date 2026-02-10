9d312f4c2c472394e7ea059cd5fcf7d2822bcb55
carlosaculsan-ui <carlosaculsan@gmail.com>
2026-02-10 13:18:54 +0800

    DataTypeSizes.java

---
+++ Java/DataTypeSizes.java
@@ -0,0 +1,14 @@
+public class MachineProblem3 {
+    public static void main(String[] args) {
+
+        System.out.println("size of char " + (Character.SIZE / 8) + " bytes ");
+        System.out.println("size of short " + (Short.SIZE / 8) + " bytes ");
+        System.out.println("size of int " + (Integer.SIZE / 8) + " bytes ");
+        System.out.println("size of long " + (Long.SIZE / 8) + " bytes ");
+        System.out.println("size of float " + (Float.SIZE / 8) + " bytes ");
+        System.out.println("size of double " + (Double.SIZE / 8) + " bytes ");
+
+    }
+
+
+}
