a50d57fb3052efe7674e0fb074b1c1a1977769ba
carlosaculsan-ui <carlosaculsan@gmail.com>
2026-02-10 13:25:43 +0800

    Create LargestNumberFinder.java

---
+++ Java/LargestNumberFinder.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class MachineProblem14 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+        System.out.print("Enter the third number: ");
+        double num3 = scanner.nextDouble();
+
+        double largest = num1;
+
+        if (num2 > largest) {
+            largest = num2;
+        }
+
+        if (num3 > largest) {
+            largest = num3;
+        }
+
+        System.out.println("The largest number is: " + largest);
+
+        scanner.close();
+    }
+}
