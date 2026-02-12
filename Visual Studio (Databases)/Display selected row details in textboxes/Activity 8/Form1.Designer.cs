fac4e9bfaad858d06069bfff12e1de31415c2ff4
carlosaculsan-ui <carlosaculsan@gmail.com>
2026-02-12 15:44:36 +0800

    Add files via upload

---
+++ Visual Studio (Databases)/Display selected row details in textboxes/Activity 8/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Activity_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtContact = new System.Windows.Forms.TextBox();
+            this.richTextAddress = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dgvUsers = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Contact";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(24, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Address";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(143, 44);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(176, 20);
+            this.txtFirstName.TabIndex = 4;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(143, 82);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(176, 20);
+            this.txtLastName.TabIndex = 5;
+            // 
+            // txtContact
+            // 
+            this.txtContact.Location = new System.Drawing.Point(143, 123);
+            this.txtContact.Name = "txtContact";
+            this.txtContact.Size = new System.Drawing.Size(176, 20);
+            this.txtContact.TabIndex = 6;
+            // 
+            // richTextAddress
+            // 
+            this.richTextAddress.Location = new System.Drawing.Point(143, 165);
+            this.richTextAddress.Name = "richTextAddress";
+            this.richTextAddress.Size = new System.Drawing.Size(176, 123);
+            this.richTextAddress.TabIndex = 7;
+            this.richTextAddress.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 311);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 55);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "SAVE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(143, 311);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(63, 55);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "DELETE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(256, 311);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(63, 55);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "CLEAR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dgvUsers
+            // 
+            this.dgvUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsers.Location = new System.Drawing.Point(345, 44);
+            this.dgvUsers.Name = "dgvUsers";
+            this.dgvUsers.Size = new System.Drawing.Size(390, 322);
+            this.dgvUsers.TabIndex = 11;
+            this.dgvUsers.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvUsers_CellClick);
+            this.dgvUsers.SelectionChanged += new System.EventHandler(this.dgvUsers_SelectionChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(747, 401);
+            this.Controls.Add(this.dgvUsers);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextAddress);
+            this.Controls.Add(this.txtContact);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtContact;
+        private System.Windows.Forms.RichTextBox richTextAddress;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dgvUsers;
+    }
+}
+
---
+++ Visual Studio (Databases)/Display selected row details in textboxes/Activity 8/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+
+namespace Activity_8
+{
+    public partial class Form1 : Form
+    {
+        private OleDbConnection connect = new OleDbConnection();
+        private string connectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\Admin\OneDrive\Documents\SDF104.mdb;Persist Security Info=False;";
+        public Form1()
+        {
+            InitializeComponent();
+            connect.ConnectionString = connectionString;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadDataIntoDataGridView();
+        }
+        private void LoadDataIntoDataGridView()
+        {
+            try
+            {
+                using (OleDbDataAdapter dataAdapter = new OleDbDataAdapter("SELECT FirstName, LastName, Contact, Address, Username, Password FROM Users", connect))
+                {
+                    DataTable dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+                    dgvUsers.DataSource = dataTable;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading data: " + ex.Message);
+            }
+        }
+
+        private void dgvUsers_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dgvUsers_SelectionChanged(object sender, EventArgs e)
+        {
+            dgvUsers.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            if (dgvUsers.SelectedRows.Count > 0)
+            {
+             
+                DataGridViewRow row = dgvUsers.SelectedRows[0];
+
+                txtFirstName.Text = row.Cells["FirstName"].Value.ToString();
+                txtLastName.Text = row.Cells["LastName"].Value.ToString();
+                txtContact.Text = row.Cells["Contact"].Value.ToString();
+                richTextAddress.Text = row.Cells["Address"].Value.ToString();
+                
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               
+                string query = "INSERT INTO Users (FirstName, LastName, Contact, Address) " +
+                               "VALUES (@FirstName, @LastName, @Contact, @Address)";
+
+                using (OleDbCommand command = new OleDbCommand(query, connect))
+                {
+                    command.Parameters.AddWithValue("@FirstName", txtFirstName.Text);
+                    command.Parameters.AddWithValue("@LastName", txtLastName.Text);
+                    command.Parameters.AddWithValue("@Contact", txtContact.Text);
+                    command.Parameters.AddWithValue("@Address", richTextAddress.Text);
+                  
+
+                    connect.Open();
+                    command.ExecuteNonQuery();
+                    connect.Close();
+
+                    MessageBox.Show("User information saved successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    LoadDataIntoDataGridView();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error saving data: " + ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dgvUsers.SelectedRows.Count > 0)
+                {
+                   
+                    int userId = Convert.ToInt32(dgvUsers.SelectedRows[0].Cells["ID"].Value);
+
+                    
+                    string query = "DELETE FROM Users WHERE ID = @ID";
+
+                    using (OleDbCommand command = new OleDbCommand(query, connect))
+                    {
+                        command.Parameters.AddWithValue("@ID", userId);
+
+                        connect.Open();
+                        command.ExecuteNonQuery();
+                        connect.Close();
+
+                        MessageBox.Show("User deleted successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        
+                        LoadDataIntoDataGridView();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please select a user to delete.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error deleting data: " + ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtFirstName.Clear();
+            txtLastName.Clear();
+            txtContact.Clear();
+            richTextAddress.Clear();
+        }
+    }
+    }
